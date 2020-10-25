6007ea36eadbd15ab3de1ecfa543c94fd6a1a04a
Domenic Catalano <domenic.catalano@dcmail.ca>
2020-10-25 14:40:23 -0400

    main with full implementation

--- OOP3200-F2020-Lesson3c/main.cpp
+++ OOP3200-F2020-Lesson3c/main.cpp
@@ -20,17 +20,12 @@ int main()
 		 ************************/
 
 		 // Declaring a map with a key type of string and a value type of Vector2D
-		std::map<std::string, Vector2D<double>*> pointObject;
+		std::map<std::string, Vector2D<int>*> pointObject;
 
 		std::string pointName;			// String to hold the name of the point
-		double x;						// Holds the x value
-		double y;						// Holds the y value
-		double totalDistance = 0.0;		// Hold the total distance between all points
-
-
-		// Using Vector2D to create two new points for comparison
-		auto* firstPoint = new Vector2D<double>(x = 0, y = 0);
-		auto* secondPoint = new Vector2D<double>(x = 0, y = 0);
+		int x;						// Holds the x value
+		int y;						// Holds the y value
+		float totalDistance = 0.0f;		// Hold the total distance between all points
 
 		/******************************************************************************
 		 *	Reading Labels/Points into the Map:
@@ -46,7 +41,7 @@ int main()
 
 		 // Declaration of an ifstream called infile with a fileName of the file we will be opening
 		std::ifstream infile;
-		std::string fileName = "MockDataForTesting.txt";
+		std::string fileName = "PointData.dat";
 
 		// Opens the file
 		infile.open(fileName.c_str());
@@ -59,27 +54,28 @@ int main()
 			{
 				std::cout << "The map is empty. Check that file contains valid data in the correct format.\n";
 			}
-
-			// While the file has not failed (failbit is not set) store the contents of the file into
-			// our pointObject
-			//int counter = 0;	// Holds value of how many points have read from the file
-			while (!infile.fail())
+			else
 			{
-				infile >> pointName;
-				infile.ignore(1, ' ');	
-				infile.ignore(1, '(');
-				infile >> x;
-				infile.ignore(1, ',');
-				infile.ignore(1, ' ');
-				infile >> y;
-				infile.ignore(1, ')');
+				// While the file has not failed (failbit is not set) store the contents of the file into
+				// our pointObject
+				while (!infile.fail())
+				{
+					infile >> pointName;
+					infile.ignore(1, ' ');
+					infile.ignore(1, '(');
+					infile >> x;
+					infile.ignore(1, ',');
+					infile.ignore(1, ' ');
+					infile >> y;
+					infile.ignore(1, ')');
 
-				auto* temp_point = new Vector2D<double>(x, y);
-				pointObject[pointName, x, y] = temp_point;
-				//++counter;
+					auto* temp_point = new Vector2D<int>(x, y);
+					pointObject[pointName] = temp_point;
+				}
+				// Close the file after all data is copied
+				infile.close();
 			}
-			// Close the file after all data is copied
-			infile.close();
+
 		}
 		// If the file does not exist, display error message to the user
 		else
@@ -98,25 +94,53 @@ int main()
 		 *	to the user how many points the map contains and what the total distance is.
 		 ******************************************************************************/
 
-		// For loop will iterate through all points and calculate the total distance between them all
-		for (auto i = pointObject.begin(); i != pointObject.end(); ++i)
+		 // For loop will iterate through all points and calculate the total distance between them all
+		for (auto itr = pointObject.begin(); /*check for condition inside the loop to avoid dereferencing end() pointer*/; ++itr)
 		{
-			// Calculate distance
-			//auto distance = Vector2D<double>::Distance(i->second.GetPosition(), (i++)->second.GetPosition());
-			std::cout << i->first << " | " << (i->second << std::endl;
-			//totalDistance += distance;
+			auto tempItr = itr;
+			++tempItr;
+			if (tempItr == pointObject.end())
+			{
+				break;
+			}
+			auto distance = Vector2D<int>::Distance(*(itr->second), *(std::next(itr)->second));
+			totalDistance += distance;
 		}
 
-		 /******************************************************************************
-		  *	Determine the Distance Between the Start Point and a User Selected Point:
-		  *	Prompt the user to enter a label or to enter "quit" to end.  If the user
-		  *	entered anything other than "quit", attempt to find the label they entered
-		  *	in the map. If it was found, report the distance between the point for the
-		  *	label they entered and the start point (i.e. the point labeled "AA").
-		  *	Otherwise, tell the user that the label they entered is not in the map.
-		  *	Repeat these steps until the user enters "quit".
-		  ******************************************************************************/
+		std::cout << "The map contains " << pointObject.size() << " and a total distance of " << totalDistance << std::endl << std::endl;
 
+		/******************************************************************************
+		 *	Determine the Distance Between the Start Point and a User Selected Point:
+		 *	Prompt the user to enter a label or to enter "quit" to end.  If the user
+		 *	entered anything other than "quit", attempt to find the label they entered
+		 *	in the map. If it was found, report the distance between the point for the
+		 *	label they entered and the start point (i.e. the point labeled "AA").
+		 *	Otherwise, tell the user that the label they entered is not in the map.
+		 *	Repeat these steps until the user enters "quit".
+		 ******************************************************************************/
+		while (true)
+		{
+			std::string input;
+			std::cout << "Enter the label of the point you wish to go to (\"quit\" to end): ";
+			std::cin >> input;
+			if (input == "quit")
+			{
+				break;
+			}
+			else
+			{
+				auto itr = pointObject.find(input);
+				if (itr == pointObject.end())
+				{
+					std::cout << "There is no point labeled \"" << input << "\" in the map." << std::endl << std::endl;
+				}
+				else
+				{
+					auto distance = Vector2D<int>::Distance(*(pointObject["AA"]), *(itr->second));
+					std::cout << "The distance between AA (0, 0) and " << itr->first << *(itr->second) << " is " << distance << std::endl << std::endl;
+				}
+			}
+		}
 	}
 	/******************************************************************************
 	 *	Exception Handling:
@@ -129,4 +153,3 @@ int main()
 	}
 	return 0;
 }
-
